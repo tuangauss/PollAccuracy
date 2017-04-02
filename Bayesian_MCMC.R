5684801e200e6077642f41e7bc670049f1b2e288
Tuan Nguyen Doan <tuandoan.nguyen@yale.edu>
2017-04-01 21:51:23 -0400

    Update Bayesian_MCMC.R

--- Bayesian_MCMC.R
+++ Bayesian_MCMC.R
@@ -11,7 +11,7 @@ for (i in 1:length(State)){
   ## we only need 2 columns, samplesize(N) and Trump_voters(X)
   smalldata <- data[,c("samplesize","Trump_voters")]
   smalldata
-  
+  }
   ### the question is, find the posterior distribution of p given value of X
   
   log_lik = function(th){
@@ -22,7 +22,7 @@ for (i in 1:length(State)){
       result = result + dbinom(x,n,th,log=TRUE)
       return (result)
     }
-  }
+
   
   ### use beta distribution for prior
   log_prior = function (th){
